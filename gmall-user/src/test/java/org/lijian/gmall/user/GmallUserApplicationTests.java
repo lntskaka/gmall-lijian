c2f024cc77a14012fc8708be79ed4a52dde33454
“lijian” <qlrjxy@126.com>
2020-03-09 11:43:00 +0800

    初始化项目工程gmall-user

---
+++ gmall-user/src/test/java/org/lijian/gmall/user/GmallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lijian.gmall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
